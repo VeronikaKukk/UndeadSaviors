bffc02f6367dc280b111ff09cdfcad17d6db0576
Veronika Kukk <veronika.kukk@ut.ee>
2023-11-27 20:50:00 +0200

    build error fixes

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 using static UnityEngine.GraphicsBuffer;
 
 public class Health : MonoBehaviour
