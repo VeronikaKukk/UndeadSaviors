c176642ef4368150f1356d0071492bff7d9d9d51
Veronika Kukk <veronika.kukk@ut.ee>
2023-12-31 21:39:56 +0200

    unit info panel made into a panel that can be moved

--- Assets/Scripts/VeronikaTest/LoadInfo.cs
+++ Assets/Scripts/VeronikaTest/LoadInfo.cs
@@ -8,18 +8,17 @@ public class LoadInfo : MonoBehaviour
     public List<UnitData> ZombieUnits;
 
     public UnitInfoCard UnitInfoCardPrefab;
-    public GameObject InfoPanel;
 
     void Start()
     {
         foreach (var unit in PlantUnits) {
-            UnitInfoCard card = Instantiate<UnitInfoCard>(UnitInfoCardPrefab, InfoPanel.transform);
+            UnitInfoCard card = Instantiate<UnitInfoCard>(UnitInfoCardPrefab, gameObject.transform);
             card.SetData(unit);
         }
 
         foreach (var unit in ZombieUnits)
         {
-            UnitInfoCard card = Instantiate<UnitInfoCard>(UnitInfoCardPrefab, InfoPanel.transform);
+            UnitInfoCard card = Instantiate<UnitInfoCard>(UnitInfoCardPrefab, gameObject.transform);
             card.SetData(unit);
         }
     }
