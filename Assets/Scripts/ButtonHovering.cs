d1739840c3ba543b7c2ba4acf19547286fffd097
HelenLeikmaa <helen.siil@protonmail.com>
2024-01-15 18:09:39 +0200

    button clicking sound added to tutorial scene

--- Assets/Scripts/ButtonHovering.cs
+++ Assets/Scripts/ButtonHovering.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ButtonHovering : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     public void PointerEnter()
     {
         transform.localScale = new Vector2(1.1f, 1.1f);
+
+
     }
 
     public void PointerExit()
     {
         transform.localScale = new Vector2(1f, 1f);
     }
+
+    public void PlayClickedSound()
+    {
+        audioSource.Play();
+    }
+
 }
